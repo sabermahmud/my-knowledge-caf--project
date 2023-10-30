189ded7702836f2b324c093c31b1226fcdc29816
sabermahmud <sabermahmud01@gmail.com>
2023-10-30 22:08:47 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-4xl font-extrabold bg-slate-300 p-4 '>Knowledge Cafe</h1> 
+    </>
+  )
+}
+
+export default App
